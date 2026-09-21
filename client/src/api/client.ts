91ab568b4aae8914a27dc9e15555dda3dbc3f3a7
yohjian <post-jbv@gmx.de>
2026-09-21 10:47:57 +0300

    fixed paths for authAPI

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -34,5 +34,5 @@ export const authApi = {
 	register: (email: string, password: string) =>
 		client.post<{ message: string }>("/auth/register", { email, password }),
 	login: (email: string, password: string) =>
-		client.post<{ message: string }>("auth/login", { email, password }),
+		client.post<{ message: string }>("/auth/login", { email, password }),
 };
