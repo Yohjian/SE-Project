25ae2249c4c5e34adccd7b881b94080048d5852f
yohjian <post-jbv@gmx.de>
2026-09-21 10:43:48 +0300

    added loading of .env

--- server/Program.cs
+++ server/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 
+DotNetEnv.Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // ── Database ──────────────────────────────────────────────────────────────────
